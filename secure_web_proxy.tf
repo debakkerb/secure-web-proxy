70b50e0cad7d89974e24f0c1b23e052a4bfd6864
Bjorn De Bakker <debakkerbjorn@gmail.com>
2025-03-02 21:29:13 +0100

    adding more resources

--- secure_web_proxy.tf
+++ secure_web_proxy.tf
@@ -1,3 +1,13 @@
+resource "google_compute_route" "swp_next_hop" {
+  project      = data.google_project.default.project_id
+  name         = "swp-next-hop"
+  network      = google_compute_network.standard.name
+  next_hop_ilb = google_network_services_gateway.default.addresses[0]
+  dest_range   = "0.0.0.0/0"
+  priority     = 100
+  tags         = ["ssh"]
+}
+
 resource "google_network_services_gateway" "default" {
   project                              = data.google_project.default.project_id
   name                                 = "secure-web-gateway"
@@ -8,7 +18,7 @@ resource "google_network_services_gateway" "default" {
   subnetwork                           = google_compute_subnetwork.resource.id
   delete_swg_autogen_router_on_destroy = true
   routing_mode                         = "NEXT_HOP_ROUTING_MODE"
-  gateway_security_policy              = google_network_security_gateway_security_policy.default.id
+  gateway_security_policy = google_network_security_gateway_security_policy.default.id
 }
 
 resource "google_network_security_gateway_security_policy" "default" {
